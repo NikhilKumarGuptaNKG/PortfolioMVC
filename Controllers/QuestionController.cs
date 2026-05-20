f74c95200135a4054558f188eb1d404bc7e0f437
NikhilKumarGuptaNKG <nikhilkumarguptankg0657@gmail.com>
2026-05-20 12:47:27 +0530

    Implement question CRUD and interview accordion UI

---
+++ Controllers/QuestionController.cs
@@ -0,0 +1,85 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using PortfolioMVC.Data;
+using PortfolioMVC.Models;
+
+namespace PortfolioMVC.Controllers
+{
+    [Authorize(Roles = "Admin")]
+    public class QuestionController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public QuestionController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        // CREATE
+        public IActionResult Create(int skillId)
+        {
+            var question = new Question
+            {
+                SkillId = skillId
+            };
+
+            return View(question);
+        }
+
+        [HttpPost]
+        public IActionResult Create(Question question)
+        {
+            _context.Questions.Add(question);
+            _context.SaveChanges();
+
+            return RedirectToAction(
+                "Details",
+                "Skill",
+                new { id = question.SkillId });
+        }
+
+        // EDIT
+        public IActionResult Edit(int id)
+        {
+            var question = _context.Questions.Find(id);
+
+            return View(question);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Question question)
+        {
+            _context.Questions.Update(question);
+            _context.SaveChanges();
+
+            return RedirectToAction(
+                "Details",
+                "Skill",
+                new { id = question.SkillId });
+        }
+
+        // DELETE
+        public IActionResult Delete(int id)
+        {
+            var question = _context.Questions.Find(id);
+
+            return View(question);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public IActionResult DeleteConfirmed(int id)
+        {
+            var question = _context.Questions.Find(id);
+
+            int skillId = question.SkillId;
+
+            _context.Questions.Remove(question);
+            _context.SaveChanges();
+
+            return RedirectToAction(
+                "Details",
+                "Skill",
+                new { id = skillId });
+        }
+    }
+}
